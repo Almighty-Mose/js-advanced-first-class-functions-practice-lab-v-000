237c5c973bccd44d8d190bfeb0b51ecb6829cc8e
Branden Mosley <branden.mosley1337@gmail.com>
2018-08-18 19:19:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solution in this file!
 function logDriverNames(drivers) {
-  return console.log(driver.name);
+  drivers.forEach(function (driver) {
+    return console.log(driver.name);
+  });
 }
